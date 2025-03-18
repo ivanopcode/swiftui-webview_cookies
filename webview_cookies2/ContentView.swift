2a6b1a3a7bcffb9a05bf86f80a4a662af5003399
Ivan Oparin <oparin@me.com>
2025-03-19 00:33:48 +0400

    Initial Commit

---
+++ webview_cookies2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  webview_cookies2
+//
+//  Created by Ivan SKL on 19.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ webview_cookies2/webview_cookies2App.swift
@@ -0,0 +1,17 @@
+//
+//  webview_cookies2App.swift
+//  webview_cookies2
+//
+//  Created by Ivan SKL on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct webview_cookies2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
